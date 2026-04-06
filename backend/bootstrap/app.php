a6ec0835b1acebc2e08bab60cf794ffa44090240
juanAndres <nacional.3456.ej@gmail.com>
2026-04-06 14:45:22 -0500

    hpta

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors; // 👈 Agrega este import
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,9 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->prepend(HandleCors::class); // 👈 Agrega esta línea
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
-
