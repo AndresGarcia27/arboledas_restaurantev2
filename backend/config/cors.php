020ae9b17723a60cb014195b8575f44e209a0285
juanAndres <nacional.3456.ej@gmail.com>
2026-04-06 13:53:50 -0500

    ultima actualizacion del cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -14,21 +14,23 @@ return [
     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     |
     */
+'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+'allowed_methods' => ['*'],
 
-    'allowed_methods' => ['*'],
+'allowed_origins' => [
+    'https://arboledas-restaurante.vercel.app', // 👈 Tu URL de Vercel (sin la barra / al final)
+    'http://localhost:5173',                   // Para que te siga funcionando en tu PC
+],
 
-    'allowed_origins' => ['https://arboledas-restaurante.vercel.app'],
+'allowed_origins_patterns' => [],
 
-    'allowed_origins_patterns' => [],
+'allowed_headers' => ['*'],
 
-    'allowed_headers' => ['*'],
+'exposed_headers' => [],
 
-    'exposed_headers' => [],
+'max_age' => 0,
 
-    'max_age' => 0,
-
-    'supports_credentials' => true,
+'supports_credentials' => true, // 👈 Ponlo en true por si usas sesiones
 
 ];
